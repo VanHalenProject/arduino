3e93d2d47c8fd6397ba2558613a2cbfac128accc
Lex van Dalen <lexdaalder@gmail.com>
2020-05-11 16:18:31 +0200

    Update VanHalenVendor.ino
    added delimiter for mqtt message to allow for vending amounts above 9

--- VanHalenVendor/VanHalenVendor.ino
+++ VanHalenVendor/VanHalenVendor.ino
@@ -1,16 +1,16 @@
 #include <UIPEthernet.h>
-#include <MQTT.h>
+#include <PubSubClient.h>
 #include <Servo.h>
 
-
+#define CLIENT_ID       "UnoMQTT"
 #define INTERVAL        3000 // 3 sec delay between publishing
 
 // Servo settings
-const int redServoPin     = 3;
-const int orangeServoPin  = 4;
-const int yellowServoPin  = 5;
-const int greenServoPin   = 6;
-const int purpleServoPin  = 7;
+int redServoPin     = 3;
+int orangeServoPin  = 4;
+int yellowServoPin  = 5;
+int greenServoPin   = 6;
+int purpleServoPin  = 7;
 
 Servo red;
 Servo orange;
@@ -24,17 +24,16 @@ Servo purple;
 // Brown  GND
 
 //MQTT settings
-const char channelName[] = "VanHalen/Vending";
-const char CLIENT_ID[] = "VanHalenVendor";
+const char* channelName = "VanHalen/Vending";
+const char* server = "192.168.1.15";
 uint8_t mac[6] = {0x00,0x01,0x02,0x03,0x04,0x05};
 EthernetClient ethClient;
-MQTTClient mqttClient;
-const char mqttServer[] = "192.168.1.15";
-const int mqttServerPort = 1883;
+PubSubClient mqttClient;
+long lastReconnectAttempt = 0;
 
 void setup() {
   // setup serial communication
-  Serial.begin(115200);
+  Serial.begin(9600);
 
   // set servo pins for each color
   red.attach(redServoPin);
@@ -48,30 +47,58 @@ void setup() {
     Serial.println(F("Ethernet configuration using DHCP failed"));
     for(;;);
   }
+  // setup mqtt client
+  mqttClient.setClient(ethClient);
+  mqttClient.setServer(server,1883);
+  mqttClient.setCallback(callback);
+  Serial.println(F("MQTT client configured"));
 
-  mqttClient.begin(mqttServer, mqttServerPort, ethClient);
-  mqttClient.onMessage(messageReceived);
-  
-  connect();
+  lastReconnectAttempt = 0;
 }
 
 void loop() {
-  mqttClient.loop();
-
   if (!mqttClient.connected()) {
-    connect();
+    long now = millis();
+    if (now - lastReconnectAttempt > 5000) { // Try to reconnect.
+      lastReconnectAttempt = now;
+      if (reconnect()) { // Attempt to reconnect.
+        lastReconnectAttempt = 0;
+      }
+    }
+  } else { // Connected.
+    mqttClient.loop();
   }
+  
   delay(1000);
 }
 
 // callback function to use incoming MQTT messages
 void callback(char* topic, byte* payload, unsigned int length) {
 
-  int rAmount = int((char)payload[0]) - 48;
-  int oAmount = int((char)payload[1]) - 48;
-  int yAmount = int((char)payload[2]) - 48;
-  int gAmount = int((char)payload[3]) - 48;
-  int pAmount = int((char)payload[4]) - 48;
+  if (length > 0){
+  String value = "";
+  for (int i=0;i<length;i++) {
+      value += (char)payload[i];
+  }
+
+  int amount[5], r=0, t=0;
+  
+  for (int i=0; i < value.length(); i++)
+  { 
+   if(value.charAt(i) == ';') 
+    { 
+     amount[t] = value.substring(r, i).toInt(); 
+     r=(i+1); 
+      t++; 
+    }
+  }
+  
+
+  int rAmount = amount[0];
+  int oAmount = amount[1];
+  int yAmount = amount[2];
+  int gAmount = amount[3];
+  int pAmount = amount[4];
  
   Serial.println("Dropping Skittles");
   Serial.print("Red: ");
@@ -94,21 +121,14 @@ void callback(char* topic, byte* payload, unsigned int length) {
 
   Serial.println("DONE");
   Serial.println("-----------------------------");
+  }
 }
 
-void connect() {
-  
-  Serial.println("Trying to connect to broker...");
-  while (!mqttClient.connect(CLIENT_ID)) {
-    Serial.print(".");
-    delay(1000);
+boolean reconnect() {
+  if (mqttClient.connect(CLIENT_ID)) {
+    mqttClient.subscribe(channelName); // Subscribe to channel.
   }
-  
-  Serial.println("Connected to MQTT broker!");
-
-  mqttClient.subscribe("VanHalen/Vending");
-  Serial.println("Connected to " + String(channelName));
-  
+  return mqttClient.connected();
 }
 
 void dropSkittles(Servo color, int amount){
@@ -120,10 +140,3 @@ void dropSkittles(Servo color, int amount){
   color.write(90);  
   }
   }
-
-void messageReceived(String &topic, String &payload) {
-  Serial.println("test");
-  Serial.println(topic);
-  Serial.println(payload);
-}
-  
